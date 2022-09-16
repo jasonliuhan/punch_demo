3b4b48bceb488a4733f54fc66d2c2010c05c5cbe
jason-huski <107972974+jason-huski@users.noreply.github.com>
2022-09-16 15:18:03 +0800

    fix:提交

--- src/views/Home/components/HomeMobile/homeMobileIos.js
+++ src/views/Home/components/HomeMobile/homeMobileIos.js
@@ -9,9 +9,8 @@ import DayUnit from "../../../../assets/home/Days.png";
 import HoursUnit from "../../../../assets/home/Hours.png";
 import MinutesUnit from "../../../../assets/home/Minutes.png";
 import SecondsUnit from "../../../../assets/home/Seconds.png";
-import ImgIos from "../../../../assets/home/1662608705628.jpg"
 
-export default class HomeMobileIos extends Component {
+export default class HomeMobileAn extends Component {
   state = {
     isshow: false,
     Days: 0,
@@ -42,23 +41,24 @@ export default class HomeMobileIos extends Component {
 
     let remianAllSeconds = Math.floor(
       (setTime.getTime() - nowTime.getTime()) / 1000
-    ); 
+    );
+
     //剩余总秒数
     let remainDays = Math.floor(remianAllSeconds / (60 * 60 * 24)); //剩余天数
-    if(remainDays < 10 ){
-      remainDays = '0'+remainDays
+    if (remainDays < 10) {
+      remainDays = "0" + remainDays;
     }
     let remainHours = Math.floor((remianAllSeconds / (60 * 60)) % 24); //剩余小时数
-    if(remainHours < 10 ){
-      remainHours = '0'+remainHours
+    if (remainHours < 10) {
+      remainHours = "0" + remainHours;
     }
     let remainMinutes = Math.floor((remianAllSeconds / 60) % 60); //剩余分钟数
-    if(remainMinutes < 10 ){
-      remainMinutes = '0'+remainMinutes
+    if (remainMinutes < 10) {
+      remainMinutes = "0" + remainMinutes;
     }
     let remainSeconds = Math.floor(remianAllSeconds % 60); //剩余秒数
-    if(remainSeconds < 10 ){
-      remainSeconds = '0'+remainSeconds
+    if (remainSeconds < 10) {
+      remainSeconds = "0" + remainSeconds;
     }
     let str = `${remainDays}:${remainHours}:${remainMinutes}:${remainSeconds}`;
     this.setState({
@@ -69,33 +69,89 @@ export default class HomeMobileIos extends Component {
     });
   };
 
-
+  // 监听
+  watchVideo = () => {
+    var elevideo = document.getElementById("video1");
+    console.log(elevideo);
+    elevideo.addEventListener(
+      "ended",
+      () => {
+        this.swiperAnimation();
+      },
+      false
+    );
+  };
 
   componentDidMount() {
+    this.watchVideo();
     setInterval(() => {
       this.showTime();
     }, 1000);
   }
 
   render() {
-   
     return (
       <div className={styles.home_page}>
         <div className={styles.headBlock}>
           <div className={styles.video_block}>
-           <img  className={`${styles.video}`} src={ImgIos} />
+            <video
+              className={`${styles.video} ${
+                this.state.isshow ? styles.show : styles.hidden
+              }`}
+              autoPlay
+              preload="auto"
+              controlsList="nodownload"
+              loop="loop"
+              muted
+              playsInline
+              data-wf-ignore="true"
+              data-object-fit="cover"
+            >
+              <source src="./loopMobile.mp4" type="video/mp4" data-wf-ignore="true" />
+              您的浏览器不支持 video 标签。
+            </video>
+
+            <video
+              id={this.state.v1}
+              className={`${styles.video} ${
+                this.state.isshow ? styles.hidden : styles.show
+              }`}
+              autoPlay
+              preload="auto"
+              controlsList="nodownload"
+              muted
+              playsInline
+              data-wf-ignore="true"
+              data-object-fit="cover"
+            >
+              <source
+                src="./startMobile.mp4"
+                type="video/mp4"
+                data-wf-ignore="true"
+              />
+              您的浏览器不支持 video 标签。
+            </video>
+            <video
+              id={this.state.v1}
+              className={`${styles.video} ${
+                this.state.isshow ? styles.hidden : styles.show
+              }`}
+              muted
+              autoPlay
+              src="./startMobile.mp4"
+            ></video>
           </div>
           <div className={styles.logoBlock}>
             <img src={LogoText} />
           </div>
           <div className={styles.timeBlock}>
-          <div className={styles.timeDown}>
+            <div className={styles.timeDown}>
               <div className={styles.timeText}>{this.state.Days}</div>
-              <div className={`${styles.fontFamily}`} >:</div>
+              <div className={`${styles.fontFamily}`}>:</div>
               <div className={styles.timeText}>{this.state.Hours}</div>
-              <div className={`${styles.fontFamily}`} >:</div>
+              <div className={`${styles.fontFamily}`}>:</div>
               <div className={styles.timeText}>{this.state.Minutes}</div>
-              <div className={`${styles.fontFamily}`} >:</div>
+              <div className={`${styles.fontFamily}`}>:</div>
               <div className={styles.timeText}>{this.state.Seconds}</div>
             </div>
             <div className={styles.timeUnit}>
